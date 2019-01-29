d59b1ff8516c3069972a75dafbd9bb5e2e51e236
AKarpetc <akarpetc@kazzinc.kz>
2019-01-29 17:55:56 +0600

    Добвалены примеры по работе с планировщиком, и библиотекой Parallel

---
+++ RikrerTestProject/MyForm.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RikrerTestProject
+{
+    public partial class MyForm : Form
+    {
+        private readonly TaskScheduler m_SyncContextTaskScheduller;
+        public MyForm()
+        {
+            InitializeComponent();
+            m_SyncContextTaskScheduller = TaskScheduler.FromCurrentSynchronizationContext();
+
+            Text = "Sunc";
+         //   Visible = true;
+            Width = 600;
+            Height = 100;
+
+        }
+
+        private CancellationTokenSource m_cts;
+        protected override void OnMouseClick(MouseEventArgs e)
+        {
+            if (m_cts != null)
+            {
+                m_cts.Cancel();
+                m_cts = null;
+                Text = "Operation canceled";
+            }
+            else
+            {
+                Text = "Operation runing";
+                m_cts = new CancellationTokenSource();
+
+                Task<int> t = Task.Run(() => Program.Sum(m_cts.Token, 2000), m_cts.Token);
+
+                t.ContinueWith(task => Text = "Result" + task.Result,
+                    CancellationToken.None,
+                    TaskContinuationOptions.OnlyOnRanToCompletion, m_SyncContextTaskScheduller);
+
+                t.ContinueWith(task => Text = "Operation Canceled",
+                    CancellationToken.None,
+                    TaskContinuationOptions.OnlyOnCanceled, m_SyncContextTaskScheduller);
+
+                t.ContinueWith(task => Text = "Operation faulted",
+                    CancellationToken.None,
+                    TaskContinuationOptions.OnlyOnFaulted, m_SyncContextTaskScheduller);
+
+            }
+
+            base.OnMouseClick(e);
+        }
+    }
+}
--- RikrerTestProject/Program.cs
+++ RikrerTestProject/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text;
@@ -43,25 +44,16 @@ namespace RikrerTestProject
 
             if (number != -1)
             {
-                dictMethod[number].Invoke(null, null);
+                var task = new Task(() => { dictMethod[number].Invoke(null, null); }, TaskCreationOptions.AttachedToParent);
+
+                task.Start();
+                task.Wait();
                 goto start;
             }
 
 
 
-            //Task parent = new Task(() =>
-            //{
-            //    var cts = new CancellationTokenSource();
-            //    var tf = new TaskFactory<Int32>(cts.Token, 
-            //        TaskCreationOptions.AttachedToParent,
-            //        TaskContinuationOptions.ExecuteSynchronously, 
-            //        TaskScheduler.Default
-            //        );
-            //    var childTask = new[]
-            //    {
-            //        tf.StartNew(()=>Sum)
-            //    };
-            //});
+
 
         }
 
@@ -71,14 +63,14 @@ namespace RikrerTestProject
         private static void RichterAutomaticlyPlayTask()
         {
             Task<int> t = Task.Run(() => Sum(CancellationToken.None, 10000));
-            Task cwr = t.ContinueWith(task => Console.WriteLine("The sum is:" + (task.IsFaulted?task.Exception.Message:task.Result.ToString()))/*,TaskContinuationOptions.OnlyOnFaulted*/);
+            Task cwr = t.ContinueWith(task => Console.WriteLine("The sum is:" + (task.IsFaulted ? task.Exception.Message : task.Result.ToString()))/*,TaskContinuationOptions.OnlyOnFaulted*/);
         }
 
         //Отмена всех автоматически запускаемых задач
         private static void RichterCanceldAutomaticlyPlayTask()
         {
             Task<int> t = Task.Run(() => Sum(10000));
-             t.ContinueWith(task => Console.WriteLine("The sum is:" + (task.IsFaulted ? task.Exception.Message : task.Result.ToString())),TaskContinuationOptions.OnlyOnRanToCompletion);
+            t.ContinueWith(task => Console.WriteLine("The sum is:" + (task.IsFaulted ? task.Exception.Message : task.Result.ToString())), TaskContinuationOptions.OnlyOnRanToCompletion);
 
             t.ContinueWith(task => Console.WriteLine("The sum is:" + (task.IsFaulted ? task.Exception.Message : task.Result.ToString())), TaskContinuationOptions.OnlyOnFaulted);
 
@@ -86,7 +78,87 @@ namespace RikrerTestProject
 
         }
 
-        static int Sum(int n)
+        //Дочерние задания
+        private static void RichterChikdernTasks()
+        {
+            Task<int[]> parent = new Task<int[]>(() =>
+            {
+                var result = new Int32[3];
+
+                new Task(() => { result[0] = Sum(1000); }, TaskCreationOptions.AttachedToParent).Start();
+                new Task(() => { result[1] = Sum(2000); }, TaskCreationOptions.AttachedToParent).Start();
+                new Task(() => { result[2] = Sum(3000); }, TaskCreationOptions.AttachedToParent).Start();
+
+                return result;
+            });
+            Console.WriteLine(parent.Status);
+            var cwt = parent.ContinueWith(parentTask => Array.ForEach(parentTask.Result, Console.WriteLine), TaskContinuationOptions.AttachedToParent);
+
+            parent.Start();
+
+        }
+
+        //Фабрика заданий
+        private static void RichterTasksFactory()
+        {
+            Task parent = new Task(() =>
+            {
+                var cts = new CancellationTokenSource();
+                var tf = new TaskFactory<Int32>(cts.Token,
+                    TaskCreationOptions.AttachedToParent,
+                    TaskContinuationOptions.ExecuteSynchronously,
+                    TaskScheduler.Default
+                    );
+
+                var childTask = new[]
+                {
+                     tf.StartNew(()=>Sum(cts.Token,1000)),
+                     tf.StartNew(()=>Sum(cts.Token,2000000)),
+                     tf.StartNew(()=>Sum(cts.Token,int.MaxValue)),
+                };
+
+                for (int task = 0; task < childTask.Length; task++)
+                {
+                    childTask[task].ContinueWith(t => cts.Cancel(), TaskContinuationOptions.OnlyOnFaulted);
+                }
+
+                tf.ContinueWhenAll(childTask, compleedTask =>
+                 compleedTask.Where(t => !t.IsFaulted && !t.IsCanceled)
+                .Max(t => t.Result), CancellationToken.None)
+                .ContinueWith(t => Console.WriteLine("The maximum is:" + t.Result),
+                TaskContinuationOptions.ExecuteSynchronously);
+
+            });
+
+            parent.ContinueWith(p =>
+            {
+                StringBuilder sb = new StringBuilder("The follow exception occured" + Environment.NewLine);
+
+                foreach (var e in p.Exception.Flatten().InnerExceptions)
+                {
+                    sb.AppendLine("" + e.GetType().ToString());
+                    Console.WriteLine(sb.ToString());
+                }
+            }, TaskContinuationOptions.OnlyOnFaulted);
+
+            parent.Start();
+        }
+
+        //Планировщик заданий
+        private static void RichterTaskScheduler()
+        {
+            MyForm myForm = new MyForm();
+            myForm.ShowDialog();
+        }
+
+        //ParalelForEach
+        private static void RichterParalelForEach()
+        {
+           Console.WriteLine( DirectoryBytes(@"D:\Xamarin", "*", SearchOption.AllDirectories));
+        }
+
+            #region HelpMethod
+            public static int Sum(int n)
         {
             int sum = 0;
             for (; n > 0; n--)
@@ -97,16 +169,59 @@ namespace RikrerTestProject
             return sum;
         }
 
-        static int Sum(CancellationToken ct, int n)
+        public static int Sum(CancellationToken ct, int n)
         {
             int sum = 0;
             for (; n > 0; n--)
             {
+
                 ct.ThrowIfCancellationRequested();
                 checked { sum += n; }
             }
-            
+            Thread.Sleep(5000);
             return sum;
         }
+
+        public static long DirectoryBytes(string path, string searchPattern, SearchOption searchOption)
+        {
+            var files = Directory.EnumerateFiles(path, searchPattern, searchOption);
+            int masterTotal=0;
+            ParallelLoopResult result = Parallel.ForEach<string, Int64>
+                (files, () =>
+                 {
+                     //Инциализация задач
+                     return 0;
+                 }, (file, loopState, index, taskLocalTotal) =>
+                 {
+                     //body вызывается один раз для каждого элемента
+                     long fileLenght = 0;
+                     FileStream fs = null;
+                     try
+                     {
+                         fs = File.OpenRead(file);
+                         fileLenght = fs.Length;
+
+                     }
+                     catch (IOException)
+                     {
+                         //Игнорирование файлов к которым нет доступа
+                     }
+                     finally
+                     {
+                         if (fs != null) fs.Dispose();
+
+                     }
+                     return taskLocalTotal + fileLenght;
+                 },
+                 taskLocalTotal => 
+                 {
+                     //localFinaly вызывается один раз в конце задания
+                     Interlocked.Add(ref masterTotal, (int)taskLocalTotal);
+                 }
+                 );
+            return masterTotal;
+        }
+        #endregion
+
     }
 }
